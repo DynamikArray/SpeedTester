e7253debc7bf438232dcf96e4afaa90224598428
DynamikArray <dynamikarray@gmail.com>
2022-01-18 22:00:49 -0500

    Added scheduled job to run every 15 minutes and save result to mongo

---
+++ client/client.js
@@ -0,0 +1,3 @@
+//  build out vue applciation to view logs and traverse / show data
+//  find a graphing library to view the data
+//  we could hook socket.io and push new log data when available for running the speed test from the client ui and having the results pipe back
---
+++ services/mongooseService.js
@@ -0,0 +1,22 @@
+const { DB_NAME } = require("../config.js");
+const mongoose = require("mongoose");
+
+var _db;
+
+const mongooseService = {
+  connectToServer: async function () {
+    mongoose.connect(`mongodb://localhost:27017/${DB_NAME}`, {
+      useNewUrlParser: true,
+      //useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
+
+    _db = mongoose.connection;
+    _db.on("error", console.error.bind(console, "mongooseService | DB connection error: "));
+    _db.once("open", function () {
+      console.log("mongooseService | DB Connected successfully");
+    });
+  },
+};
+
+module.exports = mongooseService;
