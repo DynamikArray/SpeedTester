0414581622389f6fad91f1cc84b311cefb1e9b83
DynamikArray <dynamikarray@gmail.com>
2022-01-19 01:47:49 -0500

    Updated setup script

--- config/setup.js
+++ config/setup.js
@@ -1,5 +1,14 @@
 //Accept the Oookla Liscence onetime on the machine ?
+const speedTest = require("speedtest-net");
 
-async () => {
-  await speedTest({ acceptLicense: true });
-};
+(async () => {
+  try {
+    console.log("Accept Liscene");
+    console.log(await speedTest({ acceptLicense: true }));
+    console.log("Completed");
+  } catch (err) {
+    console.log(err.message);
+  } finally {
+    process.exit(0);
+  }
+})();
