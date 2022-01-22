0b1c6ad42e6eb18339c5c3b9d897db6b0b32ad3c
DynamikArray <dynamikarray@gmail.com>
2022-01-22 01:06:13 -0500

    Working on setup script and running locally on server

--- worker/worker.js
+++ worker/worker.js
@@ -8,5 +8,8 @@ mongooseService.connectToServer();
 
 //Every Two Minutes run this job
 let speedtestInterval = SPEEDTEST_INTERVAL;
-if (process.env.NODE_ENV === "development") speedtestInterval = DEV_SPEEDTEST_INTERVAL;
+if (process.env.NODE_ENV == "development") speedtestInterval = DEV_SPEEDTEST_INTERVAL;
+
+console.log("worker | " + process.env.NODE_ENV + " Mode Enabled | SpeedTest Interval=" + speedtestInterval);
+
 const speedtestIntervalJob = schedule.scheduleJob(speedtestInterval, runSpeedTestLog);
