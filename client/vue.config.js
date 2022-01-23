9b629ec1b38ef604491fbd594960c90cad359fc4
DynamikArray <dynamikarray@gmail.com>
2022-01-23 13:50:22 -0500

    Initial Vuetify Setup and Server Config

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,4 +1,30 @@
 module.exports = {
   publicPath: "/",
   outputDir: "dist",
+
+  transpileDependencies: ["vuetify"],
 };
+
+/*
+devServer: {
+  headers: { "Access-Control-Allow-Origin": "*" },
+  contentBase: path.join(__dirname, "dist"),
+  compress: true,
+  //https: true,
+  host: "192.168.1.112",
+  port: 8081,
+  watchOptions: {
+    poll: true,
+  },
+  disableHostCheck: false,
+  proxy: {
+    "/*": {
+      target: "http://192.168.1.112:8080",
+    },
+  },
+  stats: {
+    colors: true,
+  },
+  logLevel: "error",
+},
+*/
